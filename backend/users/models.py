a36d68ffe86a2cd5cc78c1320e438ac2295c22fe
Andrew <andrew208132@gmail.com>
2023-05-10 20:29:44 +0300

    add status field to FriendshipRequest model

--- backend/users/models.py
+++ backend/users/models.py
@@ -5,13 +5,19 @@ from django.db import models
 
 class User(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    username = models.CharField(max_length=20)
+    username = models.CharField(max_length=20, unique=True)
 
 
 class FriendshipRequest(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sender")
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="receiver")
+    STATUS_CHOICES = (
+        ("P", "Pending"),
+        ("A", "Accepted"),
+        ("R", "Rejected"),
+    )
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default="P")
 
 
 class Friend(models.Model):
