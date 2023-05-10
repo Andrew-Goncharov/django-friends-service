5e910f4433e339b04dfd671d051d43424e07a712
Andrew <andrew208132@gmail.com>
2023-05-10 08:32:09 +0300

    add CreateUserView

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import status
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from .serializers import UserSerializer
+
+
+@api_view(["POST"])
+def CreateUserView(request):
+    if request.method == "POST":
+        data = request.data
+        serializer = UserSerializer(data=data)
+
+        if not serializer.is_valid():
+            return Response(
+                data=serializer.errors,
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        serializer.save()
+        return Response(
+            data=serializer.errors,
+            status=status.HTTP_201_CREATED
+        )
