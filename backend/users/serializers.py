a428629453762f8a981448877dc019b77c16085d
Andrew <andrew208132@gmail.com>
2023-05-10 20:35:55 +0300

    implement serializers

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,8 +1,34 @@
 from rest_framework import fields, serializers
-from .models import User
+from .models import User, FriendshipRequest, Friend
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ("id", "username")
+
+
+class FriendshipRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FriendshipRequest
+        fields = ("sender", "receiver")
+
+
+class FriendSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Friend
+        fields = ("friend_1", "friend_2")
+
+
+class ProcessFriendshipRequestSerializer(serializers.Serializer):
+    sender = serializers.UUIDField()
+    receiver = serializers.UUIDField()
+    action = serializers.CharField(max_length=6)
+
+    def validate_action(self, value):
+        """
+        Check if the action is correct
+        """
+        if value != "accept" and value != "reject":
+            raise serializers.ValidationError("The value is incorrect")
+        return value
