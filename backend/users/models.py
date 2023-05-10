aa871f4a8185bc8675df396c1806a7678447c802
Andrew <andrew208132@gmail.com>
2023-05-10 08:30:56 +0300

    create models and add them to the admin panel

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,20 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    username = models.CharField(max_length=20)
+
+
+class FriendshipRequest(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sender")
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="receiver")
+
+
+class Friend(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    friend_1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name="friend_1")
+    friend_2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name="friend_2")
